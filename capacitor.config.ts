593fa81ae070cd6d83775e3774c524bf3e426b74
bube009 <207951757+bube009@users.noreply.github.com>
2025-06-21 14:41:12 -0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,1 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
 
+const config: CapacitorConfig = {
+  appId: 'com.votre_entreprise.frequencequantique', // Remplacez par un identifiant unique (ex: com.monapp.frequence)
+  appName: 'Fréquence Quantique',
+  webDir: 'www', // Répertoire où seront stockés les fichiers de votre application web construite
+  bundledWebRuntime: false // Généralement false pour les applications Ionic/Angular
+};
+
+export default config;
