59bd9c54fc75f72b6cafd3892e779127efc5a872
bube009 <207951757+bube009@users.noreply.github.com>
2025-06-21 14:54:47 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
+import { routes } from './app/app.routes'; // Nous allons créer ce fichier ensuite
+import { AppComponent } from './app/app.component'; // Nous allons créer ce fichier ensuite
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes),
+  ],
+});
