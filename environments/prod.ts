d738659e12c2b29a96f0bda98622d56e4687f441
bube009 <207951757+bube009@users.noreply.github.com>
2025-06-21 22:56:56 -0400

    fix

--- environments/prod.ts
+++ environments/prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   // Ajoutez d'autres variables spécifiques à la production ici
-  apiUrl: 'https://api.votreapp.com/api' // Exemple
+  apiUrl: 
+  frequence.quantique.com/api
 };
+c
