6c41024117d4c171fbb5b4b10cd3c18c293f429d
bube009 <207951757+bube009@users.noreply.github.com>
2025-07-01 00:08:31 -0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,6 +1,10 @@
- {
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
   appId: 'com.frequency.quantique',
   appName: 'frequency.quantique',
   webDir: 'www',
   bundledWebRuntime: false
 };
+
+export default config;
